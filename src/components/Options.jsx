6ae406527c6ee15e1ed247a884ab2682ce41db97
Shartechs <shartechs@gmail.com>
2023-10-29 18:35:24 +0400

    Handling New Answers logic

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option">{option}</button>
+      {question.options.map((option, index) => (
+        <button
+          key={option}
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswered &&
+            (index === question.correctOption ? "correct" : "wrong")
+          }`}
+          disabled={hasAnswered}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
       ))}
     </div>
   );
